b8b80e30cdbd57a9859fdad042363d47c36dc333
gauravssah <124705806+gauravssah@users.noreply.github.com>
2023-10-18 17:43:22 +0530

    added

--- 03 DAY-2/forloop.cpp
+++ 03 DAY-2/forloop.cpp
@@ -3,27 +3,39 @@ using namespace std;
 
 main()
 {
-    int n;
-    cout << "Enter Your No: " << endl;
-    cin >> n;
+    // int n;
+    // cout << "Enter Your No: " << endl;
+    // cin >> n;
 
-    bool isPrimeNo = 1;
+    // bool isPrimeNo = 1;
 
-    for (int i = 2; i < n; i++)
-    {
-        if (n % i == 0)
-        {
-            isPrimeNo = 0;
-            break;
-        }
-    }
+    // for (int i = 2; i < n; i++)
+    // {
+    //     if (n % i == 0)
+    //     {
+    //         isPrimeNo = 0;
+    //         break;
+    //     }
+    // }
 
-    if (isPrimeNo == 0)
+    // if (isPrimeNo == 0)
+    // {
+    //     cout << "Not Prime No." << endl;
+    // }
+    // else
+    // {
+    //     cout << "This is a Prime No." << endl;
+    // }
+
+    // --------------------------------
+
+    for (int i = 0; i < 5; i++)
     {
-        cout << "Not Prime No." << endl;
-    }
-    else
-    {
-        cout << "This is a Prime No." << endl;
+        cout << "Hii." << endl;
+        cout << "Hello.." << endl;
+
+        continue;
+
+        cout << "Please Replay yaaarr....." << endl;
     }
 }
