460bbc0dcfce63ff78b38bed727d1c8d019cc997
gauravssah <124705806+gauravssah@users.noreply.github.com>
2023-10-18 12:38:32 +0530

    row col pringtin

--- 03 DAY-2/pattern.cpp
+++ 03 DAY-2/pattern.cpp
@@ -3,23 +3,104 @@ using namespace std;
 
 int main()
 {
+    // int n;
+    // int i = 1;
+    // cout << "Enter Your Number: " << endl;
+    // cin >> n;
+
+    // while (i <= n)
+    // {
+    //     int j = 1;
+
+    //     while (j <= n)
+    //     {
+    //         cout << j << " ";
+    //         j++;
+    //     }
+    //     cout << endl;
+    //     i++;
+    // }
+
+    // -------------------------------------------
+
+    // int n;
+    // int i = 1;
+    // cout << "Enter Your Number: " << endl;
+    // cin >> n;
+
+    // cout << "Your Pattern is:-" << endl;
+    // while (i <= n)
+    // {
+
+    //     int j = 1;
+    //     while (j <= n)
+    //     {
+    //         cout << n - j + 1 << " ";
+    //         j++;
+    //     }
+    //     cout << endl;
+    //     i++;
+    // }
+
+    // --------------------------------
+
+    // int n;
+    // int i = 1;
+    // cout << "Enter Your Number: " << endl;
+    // cin >> n;
+
+    // int count = 1;
+    // cout << "Your Pattern is:-" << endl;
+    // while (i <= n)
+    // {
+
+    //     int j = 1;
+    //     while (j <= n)
+    //     {
+    //         cout << count << " ";
+    //         count++;
+    //         j++;
+    //     }
+    //     cout << endl;
+    //     i++;
+    // }
+
+    // ---------------------------------------------
+
+    // int n;
+    // cout << "Enter You No :" << endl;
+    // cin >> n;
+    // int row = 1;
+
+    // while (row <= n)
+    // {
+    //     int col = 1;
+    //     while (col <= row)
+    //     {
+    //         cout << "*";
+    //         col++;
+    //     }
+    //     cout << endl;
+    //     row++;
+    // }
+
+    // ------------------------------------
+
     int n;
-    int i = 1;
-    cout << "Enter Your Number: " << endl;
+    cout << "Enter You No :" << endl;
     cin >> n;
+    int row = 1;
 
-    while (i <= n)
+    while (row <= n)
     {
-        int j = 1;
-
-        while (j <= n)
+        int col = 1;
+        while (col <= row)
         {
-            cout << j << " ";
-            j++;
+            cout << row;
+            col++;
         }
-
         cout << endl;
-        i++;
+        row++;
     }
 
     return 0;
