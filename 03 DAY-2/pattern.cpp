2e51aba05e36beac7e10c2e5b21b04d57e56590b
gauravssah <124705806+gauravssah@users.noreply.github.com>
2023-10-18 13:26:45 +0530

    abc

--- 03 DAY-2/pattern.cpp
+++ 03 DAY-2/pattern.cpp
@@ -86,21 +86,128 @@ int main()
 
     // ------------------------------------
 
-    int n;
-    cout << "Enter You No :" << endl;
-    cin >> n;
-    int row = 1;
+    // int n;
+    // cout << "Enter You No :" << endl;
+    // cin >> n;
+    // int row = 1;
 
-    while (row <= n)
+    // while (row <= n)
+    // {
+    //     int col = 1;
+    //     while (col <= row)
+    //     {
+    //         cout << row;
+    //         col++;
+    //     }
+    //     cout << endl;
+    //     row++;
+    // }
+
+    // ---------------------------------
+
+    // int n;
+    // cout << "Enter You No :" << endl;
+    // cin >> n;
+    // int row = 1;
+    // int count = 1;
+
+    // while (row <= n)
+    // {
+    //     int col = 1;
+    //     while (col <= row)
+    //     {
+    //         cout << count;
+    //         count++;
+    //         col++;
+    //     }
+    //     cout << endl;
+    //     row++;
+    // }
+
+    // -------------------------------------
+
+    // int n;
+    // cout << "Enter no :" << endl;
+    // cin >> n;
+    // int i = 1;
+
+    // while (i <= n)
+    // {
+    //     int j = 1;
+    //     int value = i;
+    //     while (j <= i)
+    //     {
+    //         cout << value;
+    //         value++;
+    //         j++;
+    //     }
+
+    //     cout << endl;
+    //     i++;
+    // }
+
+    // ------------------------------
+
+    // int n;
+    // cin >> n;
+    // int i = 0;
+
+    // while (i <= n)
+    // {
+
+    //     int j = 1;
+
+    //     while (j <= i)
+    //     {
+    //         cout << i - j + 1 << " ";
+    //         j++;
+    //     }
+
+    //     cout << endl;
+
+    //     i++;
+    // }
+
+    // -------------------------------------
+
+    // int n;
+    // cout << "Enter your No: " << endl;
+    // cin >> n;
+    // int i = 1;
+
+    // while (i <= n)
+    // {
+    //     int j = 1;
+    //     while (j <= n)
+    //     {
+    //         char Ch = 'A' + i - 1;
+    //         cout << Ch;
+    //         j++;
+    //     }
+    //     cout << endl;
+    //     i++;
+    // }
+
+    // ----------------------------------
+
+    int n;
+    cout << "Enter Your Number:" << endl;
+    cin >> n;
+    int i = 1;
+
+    while (i <= n)
     {
-        int col = 1;
-        while (col <= row)
+
+        int j = 1;
+        while (j <= n)
         {
-            cout << row;
-            col++;
+            char ch = 'A' + j - 1;
+            cout << ch;
+            j++;
         }
+
         cout << endl;
-        row++;
+        i++;
     }
 
     return 0;
